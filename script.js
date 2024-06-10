071b581db3555d4fe62b65aac0d95307c715147f
Clare S <Clare.Stock@gmail.com>
2024-06-10 09:09:52 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//array
+
+let products = [
+    { name: 'chapstick', price: 0.75 },
+    { name: 'cellphone charger', price: 7.50 },
+    { name: 'earbuds', price: 15.00 },
+    { name: 'Kleenex box', price: 4.00 },
+    { name: 'Nail file', price: 3.00 },
+    { name: 'Nail polish', price: 3.00 }
+]
+
+
+//add items to shopping list
+let shoppinglist = []
+function AddToList(name, price){
+    let newitem = {
+        Name: name, Price: price
+    }
+    shoppinglist.push(newitem)
+}
+
+
+
+console.log(products)
+
+function ShowList(){
+    return shoppinglist.forEach((s) => console.log(s)) 
+    };
